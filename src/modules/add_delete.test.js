afec3554a9dd30c4e8cf35955dc3b31142e442ff
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-03 11:28:59 +0300

    Updated add_delete.test
    add the delete method

--- src/modules/add_delete.test.js
+++ src/modules/add_delete.test.js
@@ -8,8 +8,7 @@ import {add} from './input.js';
   
     afterEach(() => {
       jest.clearAllMocks();
-    });
-  
+    });  
     test('Add data in index one of local storage', () => {
      add("task1");
      const data=JSON.parse(localStorage.getItem('lists'));
@@ -30,5 +29,40 @@ import {add} from './input.js';
         expect(data[2].description).toBe("task3");
         expect(data[2].completed).toBe(false);
         expect(data[2].index).toBe(3);
+       });
+});
+describe('Delete data', () => {
+    beforeEach(() => {
+      localStorage.clear();
+    });
+  
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+  
+    test('delete data in index one of local storage', () => {
+     add("task1");
+     add("task2");
+     remove(0);
+     const data=JSON.parse(localStorage.getItem('lists'));
+     expect(data[0].description).toBe("task2");
+     expect(data[0].completed).toBe(false);
+     expect(data[0].index).toBe(1);
+    });
+    test('Delete many data to local storage', () => {
+        add("task1");
+        add("task2");
+        add("task3");
+        add("task4");
+        remove(0);
+        const data=JSON.parse(localStorage.getItem('lists'));
+        expect(data[0].description).toBe("task2");
+        expect(data[0].completed).toBe(false);
+        expect(data[0].index).toBe(1);
+        remove(1);
+        const data2=JSON.parse(localStorage.getItem('lists'));
+        expect(data2[1].description).toBe("task4");
+        expect(data2[1].completed).toBe(false);
+        expect(data2[1].index).toBe(2);
        });
 });
