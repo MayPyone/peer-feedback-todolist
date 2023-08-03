f3c689214078b4d6b9e65ef5e553164f1fe0e05a
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-08-03 11:42:00 +0300

    Update update_edit_and_clear.test.js
    updated the code

--- src/modules/update_edit_and_clear.test.js
+++ src/modules/update_edit_and_clear.test.js
@@ -1,2 +1,16 @@
 import 'jest-localstorage-mock';
 import {edit} from './input.js';
+describe('Editing data', () => {
+    beforeEach(() => {
+      localStorage.clear();
+    });
+  
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+  
+    test('edit one data', () => {
+     edit(true,1);
+    });
+
+});
